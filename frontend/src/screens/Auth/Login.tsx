b97b874c00637963cce5ecea191a71d09f7ac94c
TruongChiNghiem <nghiem0859774418@gmail.com>
2025-02-13 10:28:08 +0700

    Ha xuong laravel 10

--- frontend/src/screens/Auth/Login.tsx
+++ frontend/src/screens/Auth/Login.tsx
@@ -1,9 +1,59 @@
 import React from "react";
 import { Layout } from "antd";
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import axios from 'axios';
 const Login : React.FC = () => {
+
+    const onFinish = async(values: any) => {
+        try {
+            const response = await axios.post('http://backend.test/api/login', {
+                values
+            });
+            console.log(response)
+            alert('Login successful')
+        } catch (err) {
+            alert('Invalid login ' + err);
+        }
+    }
+
     return (
         <Layout>
-            
+            <h1>Login</h1>
+            <Form
+                name="login"
+                initialValues={{ remember: true }}
+                style={{ maxWidth: 360 }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: 'Please input your Username!' }]}
+                >
+                    <Input prefix={<UserOutlined />} placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your Password!' }]}
+                >
+                    <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                    <Flex justify="space-between" align="center">
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                        <a href="">Forgot password</a>
+                    </Flex>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button block type="primary" htmlType="submit">
+                        Log in
+                    </Button>
+                    or <a href="">Register now!</a>
+                </Form.Item>
+            </Form>
         </Layout>
     )
 }
